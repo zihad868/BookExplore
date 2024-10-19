d5c7ca928933b71a9e3d85de4b805519b9b20b26
zihad868 <zihadh868@gmail.com>
2024-10-19 15:36:30 +0600

    Card Hover Effect

--- scripts/main.js
+++ scripts/main.js
@@ -6,10 +6,12 @@ function Menu(e) {
       e.name = 'close';  
       list.classList.add('top-[80px]');
       list.classList.add('opacity-100');
+      list.classList.add('z-50')
     } else {
       e.name = 'menu';   
       list.classList.remove('top-[80px]');
       list.classList.remove('opacity-100');
+      list.classList.remove('z-50'); 
     }
   }
   
@@ -56,14 +58,16 @@ function displayBooks(books) {
   books.forEach(book => {
     // Create a card for each book
     const bookCard = `
-      <div class="bg-white shadow-md rounded-lg p-4">
-        <img src="${book.formats['image/jpeg']}" alt="${book.title}" class="w-full h-64 object-cover rounded-t-md">
-        <div class="p-4">
-          <h2 class="text-xl font-bold mb-2">${book.title}</h2>
-          <p class="text-gray-700 mb-2">By: ${book.authors.map(author => author.name).join(', ')}</p>
-          <a href="${book.formats['text/html']}" class="text-cyan-500 hover:text-cyan-600 font-semibold">Read More</a>
+       <div class='transition hover:border-1 hover:scale-105'>
+        <div class="bg-white shadow-md rounded-lg p-4">
+          <img src="${book.formats['image/jpeg']}" alt="${book.title}" class="w-full h-64 object-cover rounded-t-md">
+          <div class="p-4">
+            <h2 class="text-xl font-bold mb-2">${book.title}</h2>
+            <p class="text-gray-700 mb-2">By: ${book.authors.map(author => author.name).join(', ')}</p>
+            <a href="${book.formats['text/html']}" class="text-cyan-500 hover:text-cyan-600 font-semibold">Read More</a>
+          </div>
         </div>
-      </div>
+       </div>
     `;
     
     // Append the card to the container
